6992f8907f19d13a6781631d1498203a71da5e76
RySel <104247395+Ryalynne@users.noreply.github.com>
2024-06-07 11:04:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
+        hostname: "console.cloudinary.com",
         port: "",
         pathname: "/my-account/**",
       },
