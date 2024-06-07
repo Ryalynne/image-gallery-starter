0a90f2b1b6617aff8bd172f3dda34045b8361d38
RySel <104247395+Ryalynne@users.noreply.github.com>
2024-06-07 10:59:00 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    formats: ["image/avif", "image/webp" ,"image/jpg"],
+    formats: [ "image/webp" ,"image/jpg"],
     remotePatterns: [
       {
         protocol: "https",
